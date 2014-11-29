9b21061297fca31ce148b90c5be2aebc9e92a256
dantezhu <dantezhu@qq.com>
2014-11-29 17:05:49 +0800

    先提交

--- python/flood.py
+++ python/flood.py
@@ -10,6 +10,7 @@ from netkit.box import Box
 
 import time
 import socket
+from multiprocessing import Pool
 
 
 class WSClientStream(object):
@@ -31,6 +32,89 @@ class WSClientStream(object):
         return self.sock.close(*args, **kwargs)
 
 
+class ProcessWorker(object):
+    """
+    进程worker
+    """
+    # 经过的时间
+    elapsed_time = 0
+    # 总请求，如果链接失败而没发送，不算在这里
+    transactions = 0
+    # 成功请求数
+    successful_transactions = 0
+    # 失败请求数，因为connect失败导致没发的请求也算在这里. 这3个值没有绝对的相等关系
+    failed_transactions = 0
+
+    def __init__(self, concurrent, reps, url, msg_cmd, socket_type):
+        self.concurrent = concurrent
+        self.reps = reps
+        self.url = url
+        self.msg_cmd = msg_cmd
+        self.socket_type = socket_type
+        self.stream_checker = Box().check
+        self.stream_checker = Box().check
+
+    def make_stream(self):
+        if self.socket_type == 'socket':
+            host, port = self.url.split(':')
+            address = (host, int(port))
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(address)
+            stream = Stream(s, use_gevent=True, lock_mode=0)
+        else:
+            import websocket
+            s = websocket.create_connection(self.url)
+            stream = WSClientStream(s)
+
+        return stream
+
+    def thread_worker(self, worker_idx):
+        try:
+            stream = self.make_stream()
+        except:
+            # 直接把所有的错误请求都加上
+            self.failed_transactions += self.reps
+            click.secho('thread_worker[%s] socket connect fail' % worker_idx, fg='red')
+            return
+
+        box = Box()
+        box.cmd = self.msg_cmd
+
+        send_buf = box.pack()
+        for it in xrange(0, self.reps):
+            self.transactions += 1
+            stream.write(send_buf)
+            recv_buf = stream.read_with_checker(self.stream_checker)
+            if not recv_buf:
+                click.secho('thread_worker[%s] socket closed' % worker_idx, fg='red')
+                self.failed_transactions += 1
+                break
+            else:
+                self.successful_transactions += 1
+
+    def run(self):
+        jobs = []
+
+        begin_time = time.time()
+
+        for it in xrange(0, self.concurrent):
+            job = gevent.spawn(self.thread_worker, it)
+            jobs.append(job)
+
+        gevent.joinall(jobs)
+
+        end_time = time.time()
+
+        self.elapsed_time = end_time - begin_time
+
+        return dict(
+            elapsed_time=self.elapsed_time,
+            transactions=self.transactions,
+            successful_transactions=self.successful_transactions,
+            failed_transactions=self.failed_transactions,
+        )
+
+
 class Flood(object):
 
     # 经过的时间
@@ -49,60 +133,20 @@ class Flood(object):
         self.msg_cmd = msg_cmd
         self.socket_type = socket_type
         self.process_count = process_count
-        self.stream_checker = Box().check
-
-    def make_stream(self):
-        if self.socket_type == 'socket':
-            host, port = self.url.split(':')
-            address = (host, int(port))
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(address)
-            stream = Stream(s, use_gevent=True, lock_mode=0)
-        else:
-            import websocket
-            s = websocket.create_connection(self.url)
-            stream = WSClientStream(s)
-
-        return stream
-
-    def worker(self, worker_idx):
-        try:
-            stream = self.make_stream()
-        except:
-            # 直接把所有的错误请求都加上
-            self.failed_transactions += self.reps
-            click.secho('worker[%s] socket connect fail' % worker_idx, fg='red')
-            return
-
-        box = Box()
-        box.cmd = self.msg_cmd
-
-        send_buf = box.pack()
-        for it in xrange(0, self.reps):
-            self.transactions += 1
-            stream.write(send_buf)
-            recv_buf = stream.read_with_checker(self.stream_checker)
-            if not recv_buf:
-                click.secho('worker[%s] socket closed' % worker_idx, fg='red')
-                self.failed_transactions += 1
-                break
-            else:
-                self.successful_transactions += 1
 
     def run(self):
-        jobs = []
 
-        begin_time = time.time()
+        pool = Pool(processes=self.process_count)
+        worker = ProcessWorker(self.concurrent_per_process, self.reps, self.url, self.msg_cmd, self.socket_type)
+        result_list = pool.map(worker.run, [])
 
-        for it in xrange(0, self.concurrent):
-            job = gevent.spawn(self.worker, it)
-            jobs.append(job)
+        for result in result_list:
+            self.elapsed_time += result['elapsed_time']
+            self.transactions += result['transactions']
+            self.successful_transactions += result['successful_transactions']
+            self.failed_transactions += result['failed_transactions']
 
-        gevent.joinall(jobs)
-
-        end_time = time.time()
-
-        self.elapsed_time = end_time - begin_time
+        self.elapsed_time = float(self.elapsed_time) / self.process_count
 
     @property
     def transaction_rate(self):
