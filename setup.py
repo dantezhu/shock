a23baced861f7b1d0073aa3cfa47cef4d808340b
dantezhu <dantezhu@qq.com>
2018-02-06 17:33:25 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="shock",
-    version='0.1.31',
+    version='0.1.35',
     zip_safe=False,
     platforms='any',
     packages=['shock'],
--- shock/__init__.py
+++ shock/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.31'
+__version__ = '0.1.35'
 
 from .shock_echo import ShockEcho
 from .shock_connect import ShockConnect
